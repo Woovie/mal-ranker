c456b6f38f429b8722789a87c34701b58b26d87a
Jordan Banasik <woovie@woovie.net>
2023-04-11 23:33:08 -0700

    Combined tier components into one
    Needed simplification, was overly complex prior without warrant.

--- src/components/Tier.vue
+++ src/components/Tier.vue
@@ -1,17 +1,47 @@
 <template>
   <div class="tier">
-    <div class="tierLabel">
-      <slot></slot>
+    <div class="tierLabel" :id="tier" :style="cssProps">
+      {{ tier }}
     </div>
+    <div class="tierDropzone"></div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Tier'
+    name: 'Tier',
+    props: [
+      'tier',
+      'bgcol',
+    ],
+    computed: {
+      cssProps() {
+        return {
+          '--bg-color': this.bgcol,
+        }
+      }
+    }
   }
 </script>
 
 <style>
+  .dropzone {
+    flex-grow: 1;
+  }
+</style>
 
+<style scoped>
+  .tierLabel {
+    background-color: var(--bg-color);
+    font-weight: 500;
+    color: black;
+    flex-shrink: 1;
+    padding: 0.5rem;
+    min-width: 4rem;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    min-height: 4rem;
+    max-width: 4rem;
+  }
 </style>
