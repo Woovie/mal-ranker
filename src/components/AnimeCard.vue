fbdaa14370e7f31ca94423293f6653e9ab354313
Jordan Banasik <woovie@woovie.net>
2023-04-11 23:35:18 -0700

    Add AnimeCard
    Very WIP, this code will be likely rewritten from the ground up

---
+++ src/components/AnimeCard.vue
@@ -0,0 +1,39 @@
+<script setup>
+import axios from 'axios';
+</script>
+
+<template>
+  <div>
+    <img :alt="altString" :src="url" />
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: '',
+        url: ''
+      };
+    },
+    props: [
+      'animeID',
+    ],
+    created() {
+      const results = axios.get(`/anime/${this.animeID}`, {
+        timeout: 2000,
+        responseType: 'json',
+      });
+      console.log(results);
+    },
+    computed: {
+      altString() {
+        return`Picture of ${this.title}`;
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
