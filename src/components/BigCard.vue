c08b9ff4f3857700d8328428c32070dab5914527
Jordan Banasik <woovie@woovie.net>
2023-04-27 13:25:49 -0700

    Replacement cards
    These are replacements for AnimeCard and SearchResult, I've renamed
    them as I plan to reuse them in the future for multiple purposes.
    My first idea is on hover of small card, render big card.

---
+++ src/components/BigCard.vue
@@ -0,0 +1,134 @@
+<template>
+  <div v-if="this.animeDetails && Object.keys(this.animeDetails).length > 0" @click="$emit('chose-anime', this.animeDetails)" class="searchListing">
+    <div class="imageHolder">
+      <img class="listingImage" :alt="this.animeDetails.altTag" :src="this.animeDetails.image" />
+    </div>
+    <div class="descriptionHolder">
+      <div class="listingTitle">{{ this.animeDetails.title }}</div>
+      <div class="listingDescription">{{ this.animeDetails.synopsis }}</div>
+    </div>
+  </div>
+  <div v-else class="searchListing">
+    <div class="imageHolder">
+      <div class="listingImage searchCardLoading">&nbsp;</div>
+    </div>
+    <div class="descriptionHolder">
+      <div class="listingTitle searchCardLoading">&nbsp;</div>
+      <div class="listingDescription searchCardLoading">&nbsp;</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'id'
+  ],
+  emits: [
+    'chose-anime'
+  ],
+  inject: [
+    "animeModule"
+  ],
+  data() {
+    return {
+      animeDetails: {},
+    }
+  },
+  async mounted() {
+    const details = await this.animeModule.getAnime(this.id);
+    this.animeDetails = details;
+  }
+}
+</script>
+
+<style>
+.searchListing {
+  height: 8rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding-bottom: 0.5rem;
+}
+
+.searchListing:first-child {
+  padding-top: 0.5rem;
+}
+
+.listingImage {
+  max-width: 6rem;
+  max-height: 8rem;
+  object-fit: scale-down;
+}
+
+.imageHolder {
+  width: 6rem;
+  min-width: 6rem;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.descriptionHolder {
+  height: 100%;
+  margin-left: 0.25rem;
+  flex-grow: 1;
+  padding-right: 0.25rem;
+}
+
+.listingTitle {
+  line-height: 1rem;
+  height: 1rem;
+  padding-bottom: 0.25rem;
+}
+
+.listingDescription {
+  height: 6.5rem;
+  font-size: 0.5rem;
+  overflow: hidden;
+}
+
+.searchCardLoading {
+  background-color: hsla(0, 0%, 50%, 0.2);
+  width: 100%;
+  display: inline-block;
+  position: relative;
+  overflow: hidden;
+  border-radius: 0.5rem;
+}
+
+.listingImage.searchCardLoading { 
+  height: 100%;
+}
+
+.listingImage.searchCardLoading::after {
+  animation: shimmer 2s infinite;
+}
+
+.searchCardLoading::after {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  transform: translateX(-100%);
+  background-image: linear-gradient(
+    90deg,
+    hsla(0, 0%, 0%, 0) 0,
+    hsla(0, 0%, 100%, 0.05) 25%,
+    hsla(0, 0%, 100%, 0.25) 50%,
+    hsla(0, 0%, 100%, 0.05) 75%,
+    hsla(0, 0%, 0%, 0)
+  );
+  animation: shimmer 2s infinite;
+  content: '';
+  overflow: hidden !important;
+}
+
+@keyframes shimmer {
+  100% {
+    transform: translateX(100%);
+  }
+}
+</style>
---
+++ src/components/SmallCard.vue
@@ -0,0 +1,60 @@
+<template>
+    <img @dragstart="dragStart($event)" draggable="true" class="tierCard" :alt="this.animeDetails.altTag" :src="this.animeDetails.image" v-if="this.animeDetails && Object.keys(this.animeDetails).length > 0"  />
+    <div v-else class="tierCard tierCardLoading">&nbsp;</div>
+</template>
+
+<script>
+  export default {
+    props: [
+      'animeDetails'
+    ],
+    methods: {
+      dragStart(event) {
+        event.dataTransfer.setDragImage(event.target, 0, 0);
+        event.dataTransfer.setData('animeDetails', JSON.stringify(this.animeDetails));
+      }
+    }
+  }
+</script>
+
+<style>
+.tierCard {
+  max-height: 100%;
+  object-fit: cover;
+}
+
+.tierCardLoading {
+  background-color: hsla(0, 0%, 50%, 0.2);
+  width: 100%;
+  display: inline-block;
+  position: relative;
+  overflow: hidden;
+  border-radius: 0.5rem;
+}
+
+.tierCardLoading::after {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  transform: translateX(-100%);
+  background-image: linear-gradient(
+    90deg,
+    hsla(0, 0%, 0%, 0) 0,
+    hsla(0, 0%, 100%, 0.05) 25%,
+    hsla(0, 0%, 100%, 0.25) 50%,
+    hsla(0, 0%, 100%, 0.05) 75%,
+    hsla(0, 0%, 0%, 0)
+  );
+  animation: shimmer 2s infinite;
+  content: '';
+  overflow: hidden !important;
+}
+
+@keyframes shimmer {
+  100% {
+    transform: translateX(100%);
+  }
+}
+</style>
