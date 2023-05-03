d44b971f375bc120fb4b8bb9fbe91cb3d4ac1c3a
Jordan Banasik <woovie@woovie.net>
2023-05-03 10:13:27 -0700

    Another update for serverless function

--- api/mal.js
+++ api/mal.js
@@ -1,4 +1,4 @@
 export default function handler(request, response) {
   // request.headers.append("X-MAL-CLIENT-ID", process.env.MAL_CLIENT_ID);
-  console.log(JSON.stringify(request));
+  console.log(typeof(request));
 }
